a421b1587e860b6285eb212430578e89b9698160
= <=>
2024-06-04 22:58:24 +0800

    tambah gambar dan update js buat slider

--- js/script.js
+++ js/script.js
@@ -20,6 +20,31 @@ window.addEventListener("load", function(){
     });
 });
 
+// Slide image
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n){
+    showDivs((slideIndex += n));
+}
+
+function showDivs(n){
+    var i;
+    var imgList = document.getElementsByClassName("img-slideshow");
+    if (n > imgList.length) slideIndex = 1;
+    else if (n < 1) slideIndex = imgList.length;
+
+    for (i=0; i < imgList.length; i++){
+        imgList[i].style.display = "none"
+    }
+
+    imgList[slideIndex - 1].style.display = "block";
+}
+
+setInterval(() => {
+    plusDivs(1);
+}, 5000);
+
 // Validation form
 function displayResult() {
     // Mengambil nilai dari form
